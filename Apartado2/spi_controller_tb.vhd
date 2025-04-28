b3148816471e1a49f73703f532449bb76c844704
Gonzalo Todó Gabino <gonzalo.todo@edu.uah.es>
2025-04-28 12:21:22 +0200

    Simulacion funcional hecha
    He implementado pero no me aparece la simulacion temporal

--- Apartado2/spi_controller_tb.vhd
+++ Apartado2/spi_controller_tb.vhd
@@ -1,26 +1,38 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity spi_controller_tb is
-
 end spi_controller_tb;
 
 architecture sim of spi_controller_tb is
 
-  signal CLK_i         : std_logic                     := '0';
-  signal RST_i         : std_logic                     := '1';
-  signal DATA_SPI_OK_i : std_logic                     := '0';
-  signal DATA_SPI_i    : std_logic_vector (8 downto 0) := (others => '0');
+  signal CLK_i         : std_logic := '0';
+  signal RST_i         : std_logic := '1';
+  signal DATA_SPI_OK_i : std_logic := '0';
+  signal DATA_SPI_i    : std_logic_vector(8 downto 0) := (others => '0');
+  signal D_C_i         : std_logic;
+  signal CS_i          : std_logic;
+  signal SDIN_i        : std_logic;
+  signal SCLK_i        : std_logic;
+  signal END_SPI_i     : std_logic;
 
-  signal   D_C_i     : std_logic;
-  signal   CS_i      : std_logic;
-  signal   SDIN_i    : std_logic;
-  signal   SCLK_i    : std_logic;
-  signal   END_SPI_i : std_logic;
-  constant T_data    : time := 1ms; --Completar (est� mal puesto el valor)
-begin  -- sim
+  constant T_clk  : time := 10 ns;     -- Periodo de CLK (100 MHz)
+  constant T_data : time := 20 us;     -- Tiempo entre envíos de datos
 
-  DUT : entity work.spi_controller
+begin
+
+  -- Generación del reloj
+  clk_process : process
+  begin
+    CLK_i <= '0';
+    wait for T_clk/2;
+    CLK_i <= '1';
+    wait for T_clk/2;
+  end process;
+
+  -- Instancia del DUT: spi_controller
+  DUT: entity work.spi_controller
     port map (
       CLK         => CLK_i,
       RST         => RST_i,
@@ -30,24 +42,20 @@ begin  -- sim
       CS          => CS_i,
       SDIN        => SDIN_i,
       SCLK        => SCLK_i,
-      END_SPI     => END_SPI_i);
+      END_SPI     => END_SPI_i
+    );
 
-
+  -- Instancia del dispositivo SPI simulado
   SPI_DEV : entity work.spi_device
-    port map (
+    port map(
       D_C  => D_C_i,
       CS   => CS_i,
       SDIN => SDIN_i,
-      SCLK => SCLK_i);
+      SCLK => SCLK_i
+    );
 
-
-  -- estímulos para CLK y RST
-
-  RST_i <= '0' after 123ns;                             --completar, copiado del tutorial
-  CLK_i <= not clk_i after 5ns;                             --completar
-
-
-  process
+  -- Estímulos de la simulación
+  stimulus : process
     procedure gen_dato(dato : std_logic_vector(8 downto 0)) is
     begin
       wait until CLK_i = '0';
@@ -56,24 +64,23 @@ begin  -- sim
       wait until CLK_i = '0';
       DATA_SPI_OK_i <= '0';
       wait for T_data;
-    end gen_dato;
+    end procedure;
+  begin
+    -- Reset inicial
+    wait for 100 ns;
+    RST_i <= '0';
 
-  begin  -- process
+    -- Enviar varios datos
+    gen_dato("0" & x"48"); -- H
+    gen_dato("0" & x"45"); -- E
+    gen_dato("0" & x"4C"); -- L
+    gen_dato("0" & x"4F"); -- O
 
-    --wait for ;--Completar
-   -- gen_dato(''&x"");--Completar
-   -- gen_dato(''&x"");--Completar
-    --gen_dato(''&x"");--Completar
-    --gen_dato(''&x"");--Completar
-    --gen_dato(''&x"");--Completar
-    --gen_dato(''&x"");--Completar
-   --gen_dato(''&x"");--Completar
---gen_dato(''&x"");--Completar
-    --gen_dato(''&x"");--Completar   
-
-    report "fin de la simulacion" severity failure;
+    wait for 500 us;
+    report "Fin de la simulación" severity failure;
   end process;
 
 end sim;
+
 
 -------------------------------------------------------------------------------
